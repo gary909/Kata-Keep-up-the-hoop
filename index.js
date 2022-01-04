7227af2b5ca09b8a06055407a9a21998ab01daff
gary <gary.white@lodgit.com>
2022-01-04 13:07:30 +0100

    boilerplate

---
+++ index.js
@@ -0,0 +1,6 @@
+function hoopCount (n) {
+    //your code goes here    
+}
+
+console.log(hoopCount(3)); // Return "Keep at it until you get it"
+console.log(hoopCount(11)); // Return "Great, now move on to tricks"
